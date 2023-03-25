95865329d441b2481c39f947df25ace45faef37d
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-25 20:41:20 +0530

    Factorial in JavaScript

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 alert("Welcome to JavaScript World................");
+
 alert('For Loop Example');
 for(let i=1;i<=10;i++) {
   alert(i);  
 }
+
+alert('Factorial in JavaScript')
+let number = 7;
+let fact = 1;
+for (i = 1; i <= number; i++) {
+    fact *= i;
+}
+alert(fact)
