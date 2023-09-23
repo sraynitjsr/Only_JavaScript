99840cbd033c06b66483599d16dc2002c1a0ab68
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-23 10:22:10 +0530

    Functions in JavaScript

--- main.js
+++ main.js
@@ -21,3 +21,9 @@ let colors = ["red", "green", "blue"];
 for (let i = 0; i < colors.length; i++) {
   console.log(colors[i]);
 }
+
+function display(name) {
+  console.log("Hello, " + name + "!");
+}
+
+display("Ray");
