64d46ce00967fdcc8bdb594060a39ea22688c141
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-23 10:24:54 +0530

    Arrays in JavaScript

--- main.js
+++ main.js
@@ -34,3 +34,46 @@ let person = {
   isStudent: true,
 };
 console.log(person.name + " is " + person.age + " years old.");
+
+console.log("Learning JavaScript As A Programming Language");
+
+let name = "Sachin";
+let age = 37;
+console.log(name + " is " + age + " years old.");
+
+let number = 2020;
+let text = "Hello";
+let isTrue = true;
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(number + " " + text + " " + isTrue + " Array => " + fruits);
+
+let temperature = 15;
+if (temperature > 25) {
+  console.log("It's hot outside!");
+} else {
+  console.log("It's not too hot.");
+}
+
+let colors = ["red", "green", "blue"];
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Ray");
+
+let person = {
+  name: "Tufan",
+  age: 30,
+  isStudent: true,
+};
+console.log(person.name + " is " + person.age + " years old.");
+
+let numbers = [1, 2, 3, 4, 5];
+let doubled = numbers.map(function (num) {
+  return num * 2;
+});
+console.log(doubled);
