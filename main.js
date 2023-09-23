7f25750461e486a6fc68aaff7315f748f9998db1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-23 10:18:37 +0530

    Data Types in JavaScript

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ console.log("Learning JavaScript As A Programming Language");
 let name = "Sachin";
 let age = 37;
 console.log(name + " is " + age + " years old.");
+
+let number = 2020;
+let text = "Hello";
+let isTrue = true;
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(number + " " + text + " " + isTrue + " Array => " + fruits);
