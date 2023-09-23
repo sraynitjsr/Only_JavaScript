21e3ffae40f4253cd6d1d82143887eff72066d83
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-23 10:20:13 +0530

    Conditional Statements in JavaScript

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ let text = "Hello";
 let isTrue = true;
 let fruits = ["Apple", "Banana", "Cherry"];
 console.log(number + " " + text + " " + isTrue + " Array => " + fruits);
+
+let temperature = 15;
+if (temperature > 25) {
+  console.log("It's hot outside!");
+} else {
+  console.log("It's not too hot.");
+}
