90332357c948394a2938d3c8f8d149a14df88582
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-12-28 03:31:45 +0530

    Array Transformation and Map Using JavaScript

---
+++ 8_Array_Transformation_Map.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5];
+
+let squaredNumbers = numbers.map(function(num) {
+    return num * num;
+});
+
+console.log("Original Numbers:", numbers);
+console.log("Squared Numbers:", squaredNumbers);
